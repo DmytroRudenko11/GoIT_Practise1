8a4a2adfb6e408580283a53b79a3b3381fd4191c
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-03-27 15:39:51 +0300

    add localeStorage

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,17 +1,25 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigaion from './Navigation';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setAuthToken } from '../store/auth/index';
+import { getTokenFromLocaleStorage } from 'src/store/services/authServise';
 
 export default function Wrapper() {
-  const authToken = useSelector(state => state.auth.authToken);
+  const authToken = useSelector((state) => state.auth.authToken);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!authToken) {
+    const token = getTokenFromLocaleStorage();
+    token && dispatch(setAuthToken(token));
+
+    if (!token) {
       navigate('/login');
     }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authToken]);
 
   return (
--- src/store/auth/operations.js
+++ src/store/auth/operations.js
@@ -1,27 +1,30 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { httpServer } from "../../api";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { httpServer } from '../../api';
+import { setTokenToLocaleStorage, removeTokenFromLocaleStorage } from '../services/authServise';
 
-export const loginOperation = createAsyncThunk("auth/login", async (data) => {
-  const response = await httpServer.post("/auth/signin", data);
+export const loginOperation = createAsyncThunk('auth/login', async (data) => {
+  const response = await httpServer.post('/auth/signin', data);
+  setTokenToLocaleStorage(response.data.data);
   return response.data.data;
 });
 
-export const logoutOperation = createAsyncThunk(
-  "auth/logout",
-  async (data, { getState }) => {
-    const state = getState();
+export const logoutOperation = createAsyncThunk('auth/logout', async (data, { getState }) => {
+  const state = getState();
 
-    await httpServer.post("/auth/signout", data, {
-      headers: { Authorization: state.auth.authToken },
-    });
-    return;
+  const response = await httpServer.post('/auth/signout', data, {
+    headers: { Authorization: state.auth.authToken },
+  });
+  if (response.data.status === 'logout') {
+    removeTokenFromLocaleStorage();
   }
-);
+
+  return;
+});
 
 export const registrationOperation = createAsyncThunk(
-  "auth/registration",
+  'auth/registration',
   async (data, { dispatch }) => {
-    const response = await httpServer.post("/auth/signup", data);
+    const response = await httpServer.post('/auth/signup', data);
 
     if (response.data.user) {
       dispatch(
